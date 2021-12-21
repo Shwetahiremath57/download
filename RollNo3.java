c5dd646e4852f1895628dcbb24b7cef44946ef76
Shwetahiremath57 <shwetahiremath17xworkz@gmail.com>
2021-12-21 10:11:32 +0530

    added download java file

---
+++ RollNo3.java
@@ -0,0 +1,16 @@
+class RollNo3{
+                             public static void main(String []a){
+                             short [] ids= {5,10,15,20,25,30,2,33,7,2};
+                             System.out.println(ids[0]);
+                             System.out.println(ids[1]);
+                             System.out.println(ids[2]);
+                             System.out.println(ids[3]);
+                             System.out.println(ids[4]);
+                             System.out.println(ids[5]);
+                             System.out.println(ids[6]);
+                             System.out.println(ids[7]);
+                             System.out.println(ids[8]);
+                             System.out.println(ids[9]);
+               }
+}                           
+                             
